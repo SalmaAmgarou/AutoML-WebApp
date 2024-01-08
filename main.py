0dd34b413af02e35def9e26bc6d6b1b3de3bfa62
hafdaoui <lilpump0629@gmail.com>
2024-01-09 00:50:53 +0100

    12;50

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 from user_interface.window_main import display_data_information , display_missing_values , display_descriptive_statistics
 from machine_learning.data_management.data_loader import load_data
 from machine_learning.data_management.data_preprocessing import select_target
-from machine_learning.data_management.Visualization import visualize_data , create_correlation_heatmap
-from images import  *
-
+from machine_learning.data_management.Visualization import visualize_data
 if __name__ == "__main__":
     st.set_page_config(page_title="Machine Learning in Action", page_icon="💾")
-    col1, col2 = st.columns(2)
-with col1:
-    st.image('/home/hafdaoui/PycharmProjects/RAFINHA/images/LOGO IADS FR.png',caption=None, width=30)
-
-# Right-aligned image
-with col2:
-    st.image('/home/hafdaoui/PycharmProjects/RAFINHA/images/fstt.png',caption=None, width=30)
+    col1, col2 = st.columns([1, 1])  # Two columns of equal width
+    with col1:
+        st.image('images/LOGO IADS FR.png',  width=240)
+    with col2:
+        st.image('images/fstt.png',  width=420)
     tab1, tab2 = st.tabs(["Upload Data", "Visualize Data"])
     df = None
     with tab1:
@@ -25,17 +20,10 @@ with col2:
 
             with columns[0]:
                 display_data_information(df)
-
-
             with columns[1]:
                 display_missing_values(df)
-
-
             display_descriptive_statistics(df)
             select_target(df)
 
-
-
     with tab2:
         visualize_data(df)
-
