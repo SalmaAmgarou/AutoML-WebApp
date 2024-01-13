855a6038ee5943e718c9146de5094cf152189cb2
hafdaoui <lilpump0629@gmail.com>
2024-01-14 00:46:23 +0100

    00:46

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import streamlit as st
 from user_interface.window_main import display_data_information, display_missing_values, display_descriptive_statistics
 from machine_learning.data_management.data_loader import load_data
@@ -19,11 +20,12 @@ from streamlit_option_menu import option_menu as om
 
 
 
-
+# Main function
 def main():
-
+    # Set page configuration and title
     st.set_page_config(page_title="Machine Learning in Action", page_icon="💾", menu_items={
         'About': "### Welcome to our Machine Learning in Action. \n *Version 1.0* - Created with passion by HAMZA HAFDAOUI [Hamza Hafdaoui on GitHub](https://github.com/HAMZAUEST) and SALMA AMGAROU.   [Salma Amgarou on GitHub](https://github.com/SalmaAmgarou) \n What is this? \n **Explore the fascinating world of machine learning through our Streamlit app! We've curated an interactive experience that allows you to witness machine learning algorithms in action."})
+    # Styling with custom fonts
     st.markdown("""
         <style>
         @import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
@@ -65,14 +67,14 @@ def main():
     with col2:
         st.image('images/fstt.png',  width=420)
     st.markdown('<p class="title">Machine Learning In Action</p>', unsafe_allow_html=True)
+    # Option menu for selecting different sections
     selected = om("", ['', ' '], icons=['house', 'book', 'list-task', 'gear'], menu_icon='cast', default_index=0, orientation='horizontal')
     selected
+    # Main section handling different tabs
     if selected == '':
-
-
-
+        # Create tabs for different functionalities
         tab1, tab2, tab3 = st.tabs(["Upload Data", "Visualize Data", "Data Modeling"])
-
+        # Session state initialization
         if 'original_df' not in st.session_state:
             st.session_state.original_df = None
         if 'preprocessed_df' not in st.session_state:
@@ -80,15 +82,18 @@ def main():
         if 'model' not in st.session_state:
             st.session_state.model = None  # Initialize model attribute
 
+        # Tab 1: Upload Data
         with tab1:
             with st.status("Downloading data..."):
+                # Simulate data loading delay
                 time.sleep(0.8)
                 st.session_state.original_df = load_data()
 
-
+            # Check if data is loaded successfully
             if st.session_state.original_df is not None:
                 st.success('Data Loaded Successfully!')
                 st.write(":red[Switch to the 'Visualize Data' tab to see the visualizations.]")
+                # Sidebar for data preprocessing options
                 columns = st.columns(2)
                 if st.session_state.preprocessed_df is None:
                     preprocessed_df = st.session_state.original_df.copy()
@@ -97,21 +102,30 @@ def main():
                 st.sidebar.markdown('<p class="divider">####################</p>', unsafe_allow_html=True)
                 st.sidebar.markdown('<p class="dot-matrix">Features selection</p>', unsafe_allow_html=True)
                 st.sidebar.markdown('<p class="divider">####################</p>', unsafe_allow_html=True)
+
+                # Enable column deletion checkbox
                 delete_columns_checkbox = st.checkbox("Enable Column Deletion")
                 if delete_columns_checkbox:
                     preprocessed_df = delete_columns(preprocessed_df)
+
+                # Handle missing values (numeric) checkbox
                 missing_values_numeric = st.sidebar.checkbox("Handle Missing Values (Numeric)")
                 if missing_values_numeric:
                     preprocessed_df = handle_missing_values_numeric(preprocessed_df)
+
+                # Handle missing values (categorical) checkbox
                 missing_values_categorical = st.sidebar.checkbox("Handle Missing Values (Categorical)")
                 if missing_values_categorical:
                     preprocessed_df = handle_missing_values_categorical(preprocessed_df)
+                # Handle outliers checkbox
                 handle_out = st.sidebar.checkbox("Handle Outliers")
                 if handle_out:
                     preprocessed_df = handle_outliers(preprocessed_df, threshold=3.0)
                 st.sidebar.markdown('<p class="divider">####################</p>', unsafe_allow_html=True)
                 st.sidebar.markdown('<p class="dot-matrix">Data transformation</p>', unsafe_allow_html=True)
                 st.sidebar.markdown('<p class="divider">####################</p>', unsafe_allow_html=True)
+
+                # Encoding categorical and numeric checkbox
                 encoding = st.sidebar.checkbox("Encoding categorical and numeric")
                 if encoding:
                     preprocessed_df = encoding_categorical(preprocessed_df)
@@ -119,8 +133,11 @@ def main():
                 if scaling:
                     preprocessed_df = scaler(preprocessed_df)
 
+                # Feature scaling checkbox
                 st.header("")
                 st.data_editor(preprocessed_df)
+
+                # Display preprocessed data in data editor
                 st.header("")
                 col2, col3 = st.columns([1, 1])
                 with col2:
@@ -128,87 +145,34 @@ def main():
                 with col3:
                     display_missing_values(preprocessed_df)
                 display_descriptive_statistics(preprocessed_df)
-
+                # Save preprocessed data in session state
                 st.session_state.preprocessed_df = preprocessed_df
-
+        # Tab 2: Visualize Data
         with tab2:
             visualize_data(st.session_state.preprocessed_df)
+
+        # Tab 3: Data Modeling
         with tab3:
             st.header("")
             st.markdown('<p class="titles">Data Modeling</p>', unsafe_allow_html=True)
             st.header("")
 
+            # Check if preprocessed data is available
             if st.session_state.preprocessed_df is None:
                 st.warning("Please upload and preprocess data first.")
             else:
+                # Radio button to select the task (Classification, Regression, Clustering)
                 task = st.radio("Select Task", ["Classification", "Regression", "Clustering"])
+
+                # Select model and train
                 select_model_and_train(st.session_state.preprocessed_df, task)
+
+    # Documentation section
     elif selected == ' ':
-        st.title("Documentation and Tutorials")
-
-        st.header("Code Comments and Docstrings")
-        st.markdown("""
-        - Ensure that the code is well-commented. Use comments to explain complex or non-trivial sections of the code.
-        - Utilize docstrings for functions and classes, providing information on parameters, return values, and the purpose of the function or class.
-        """)
-
-        st.header("Separate Documentation Section")
-        st.markdown("""
-        - Create a dedicated section for documentation and tutorials accessible through a navigation menu or a separate tab.
-        - Use Markdown for clear and organized documentation.
-        """)
-
-        st.header("Tutorials")
-        st.markdown("""
-        - Provide step-by-step tutorials for common tasks and workflows.
-        - Include a combination of text, code snippets, and visuals to guide users through the process.
-        """)
-
-        st.header("Algorithm Explanations")
-        st.markdown("""
-        - Include detailed explanations for each implemented algorithm.
-        - Describe the purpose, functionality, and relevant parameters of each algorithm.
-        - Provide references to external resources or papers for further reading.
-        """)
-
-        st.header("Interactive Examples")
-        st.markdown("""
-        - Use Streamlit's interactive features to create examples that users can manipulate in real-time.
-        - Enhance tutorials with interactive elements to improve user engagement.
-        """)
-
-        st.header("Sample Datasets")
-        st.markdown("""
-        - Include sample datasets that users can use to follow along with tutorials and test the application's functionalities.
-        - Provide information on the structure and content of sample datasets.
-        """)
-
-        st.header("User Feedback")
-        st.markdown("""
-        - Allow users to provide feedback on documentation and tutorials.
-        - Implement a feedback mechanism to gather user opinions and suggestions.
-        """)
-
-        st.header("Version Control")
-        st.markdown("""
-        - Ensure that documentation is updated with each version of the application.
-        - Users should be able to access documentation corresponding to the version they are using.
-        """)
-
-        st.header("Additional Resources")
-        st.markdown("""
-        - Provide links to external resources or documentation for related topics, such as machine learning concepts, Streamlit usage, or data science best practices.
-        """)
-
-        st.header("Community Support")
-        st.markdown("""
-        - Consider creating a community forum or space for users to ask questions, share tips, and discuss the application.
-        - Foster a sense of community and provide additional support through user interactions.
-        """)
-
-        st.success("Documentation and tutorials have been outlined. Customize them based on your application's specifics.")
+        st.header("docs")
 
 
+# Run the main function if the script is executed directly
 if __name__ == "__main__":
     main()
 
--- user_interface/window_main.py
+++ user_interface/window_main.py
@@ -1,25 +1,37 @@
 import streamlit as st
 import pandas as pd
+
+# Function to display data information
 def display_data_information(dataframe):
+    # Displaying a header
     st.header("")
+    # Using markdown for styling the title
     st.markdown('<p class="titles">Data Information</p>', unsafe_allow_html=True)
+    # Displaying the number of rows, columns, and data types of the dataframe
     st.write("Number of Rows:", dataframe.shape[0])
     st.write("Number of Columns:", dataframe.shape[1])
     st.write("Data Types:", dataframe.dtypes)
 
 # Function to display missing values
 def display_missing_values(dataframe):
+    # Displaying a header
     st.header("")
+    # Using markdown for styling the title
     st.markdown('<p class="titles">Missing Values</p>', unsafe_allow_html=True)
+    # Calculating missing values for each colum
     missing_values = dataframe.isnull().sum()
+    # Filtering columns with missing values
     columns_with_missing_values = missing_values[missing_values > 0]
 
+    # Checking if there are any missing values
     if columns_with_missing_values.empty:
         st.write("No missing values found in columns.")
     else:
         st.write("Columns with missing values:")
+        # Displaying columns with missing values and their respective counts
         for column, count in columns_with_missing_values.items():
             st.text(f"Column: '{column}' - Missing values: {count}")
+    # Displaying the overall missing values count for each column
     st.write(missing_values)
 
 # Function to display descriptive statistics
@@ -27,4 +39,5 @@ def display_descriptive_statistics(dataframe):
     st.header("")
     st.markdown('<p class="titles">Descriptive Statistics</p>', unsafe_allow_html=True)
     st.header("")
+    # Displaying the descriptive statistics of the dataframe
     st.write(dataframe.describe())
